defceb9fceb4636640bc4bdae7f800a8a2d3658a
jadersonbarbosa <jadersonaraujo@yahoo.com.br>
2026-05-22 13:10:47 -0300

    adicionado login do google e trave de segurança

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Cole aqui dentro as suas chaves reais do painel do Supabase
+const supabaseUrl = "https://rjpcayffszhknjnqmfew.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqcGNheWZmc3poa25qbnFtZmV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MjQzOTAsImV4cCI6MjA5NTAwMDM5MH0.0bhrvaAf9_8egJQzCFRwcP-z5Sprv-clFKbysf9HkrA";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
